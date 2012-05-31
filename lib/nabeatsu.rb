714cbee8ca609e267cc616b1f9b7aeb8c002023f
Masato Ikeda <masato.ikeda@gmail.com>
2012-05-31 16:23:25 +0900

    幸せになれた

--- lib/nabeatsu.rb
+++ lib/nabeatsu.rb
@@ -1,12 +1,19 @@
 # coding: utf-8
 class Nabeatsu
   def say(n)
-    if n % 3 == 0
-      return "hoge"
-    elsif n.to_s =~ /3/
-      return "hoge"
-    else
+    w = []
+    if n % 3 == 0 || n.to_s =~ /3/
+      w << "hoge"
+    end
+
+    if n % 8 == 0
+      w << "fuga"
+    end
+
+    if w.size == 0
       n.to_s
+    else
+      w.join(" ")
     end
   end
 end
