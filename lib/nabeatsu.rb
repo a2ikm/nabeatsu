e79d1782a272690f2006ab1c1688f7682e1999c3
Masato Ikeda <masato.ikeda@gmail.com>
2012-05-31 16:16:03 +0900

    frist implementation

--- lib/nabeatsu.rb
+++ lib/nabeatsu.rb
@@ -1,5 +1,13 @@
 # coding: utf-8
 class Nabeatsu
-
+  def say(n)
+    if n % 3 == 0
+      return "hoge"
+    elsif n.to_s =~ /3/
+      return "hoge"
+    else
+      n.to_s
+    end
+  end
 end
 
--- spec/lib/nabeatsu_spec.rb
+++ spec/lib/nabeatsu_spec.rb
@@ -2,6 +2,28 @@
 require File.dirname(__FILE__)+"/../spec_helper"
 
 describe Nabeatsu do
-
+  describe "#say" do
+    subject { Nabeatsu.new.say(n) }
+    context "with 1" do
+      let(:n) { 1 }
+      it { should == "1" }
+    end
+    context "with 3" do
+      let(:n) { 3 }
+      it { should == "hoge" }
+    end
+    context "with 6" do
+      let(:n) { 6 }
+      it { should == "hoge" }
+    end
+    context "with 13" do
+      let(:n) { 13 }
+      it { should == "hoge" }
+    end
+    context "with 31" do
+      let(:n) { 31 }
+      it { should == "hoge" }
+    end
+  end
 end
 
